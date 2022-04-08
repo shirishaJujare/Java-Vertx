0dbcc96e7e6d5bc579234999fb198d915147f60b
shirishaJujare <97426375+shirishaJujare@users.noreply.github.com>
2022-04-08 09:11:12 +0530

    Update Dao.java

--- employee/src/main/java/com/torryharris/employee/crud/dao/Dao.java
+++ employee/src/main/java/com/torryharris/employee/crud/dao/Dao.java
@@ -2,6 +2,7 @@ package com.torryharris.employee.crud.dao;
 
 import com.torryharris.employee.crud.model.Employee;
 import io.vertx.core.Promise;
+import io.vertx.ext.web.RoutingContext;
 
 import java.util.List;
 
@@ -11,9 +12,14 @@ public interface Dao<T> {
 
   Promise<List<T>> getAll();
 
+//  void login(String username,String password);
+
+  Promise<List<Employee>> login(String username,String password);
+
+
    void save(Employee e);
 
- void update(String id);
+ void update(Employee e);
 
   void delete(String id);
 
