b22284b8971734d78c5e350ee50a052a8e816892
shirishaJujare <97426375+shirishaJujare@users.noreply.github.com>
2022-04-08 09:11:55 +0530

    Update QueryNames.java

--- employee/src/main/java/com/torryharris/employee/crud/util/QueryNames.java
+++ employee/src/main/java/com/torryharris/employee/crud/util/QueryNames.java
@@ -6,4 +6,6 @@ public class QueryNames {
   public static final String INSERT_EMPLOYEE = "INSERT_EMPLOYEE";
   public static final String UPDATE_EMPLOYEE = "UPDATE_EMPLOYEE";
   public static final String DELETE_EMPLOYEE = "DELETE_EMPLOYEE";
+  public static final String LOGIN = "LOGIN";
+
 }
